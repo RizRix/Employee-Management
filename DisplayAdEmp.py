ee5cb92469cba99603eee5390167f861af298331
Rizwana Abdul Salam <rizu400@gmail.com>
2021-11-07 08:06:47 +0530

    Update DisplayAdEmp.py

--- DisplayAdEmp.py
+++ DisplayAdEmp.py
@@ -58,7 +58,7 @@ def r():
 
 
 
-# getting data from MySQL student table
+
     cr.execute('select * from eemp')
     for dt in cr:
         trv.insert("", 'end',text=dt[0],values=(dt[0], dt[1], dt[2], dt[3], dt[4], dt[5], dt[6]))
@@ -111,7 +111,7 @@ def abcd():
     x.commit()
     tkinter.messagebox.showinfo("SUCCESSFULL!", "INSERTED")
 
-    # call(['python', 'nxt.py'])
+   
 
 
 Button(text="Submit", command=abcd, bg="black", fg="black").place(x=120, y=440)
